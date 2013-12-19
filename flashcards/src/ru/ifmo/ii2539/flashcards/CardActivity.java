6f9e58e33a5893ad819ef0045948a6de1e1e3f98
Live session user <mint@mint.(none)>
2013-12-19 07:13:02 +0000

    User answer implemented

--- flashcards/src/ru/ifmo/ii2539/flashcards/CardActivity.java
+++ flashcards/src/ru/ifmo/ii2539/flashcards/CardActivity.java
@@ -1,32 +1,40 @@
 package ru.ifmo.ii2539.flashcards;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CardActivity extends Activity {
 
+    private int cardNumber;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.card);
-        int category = getIntent().getExtras().getInt("category");
+        showCard(getIntent().getExtras().getInt("category"));
+    }
+
+    void showCard(int category) {
         int number = 0;
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
         Drawable image = getResources().getDrawable(getResources().getIdentifier("i" + Integer.toString(category) + Integer.toString(number), "drawable", getPackageName()));
         imageView.setImageDrawable(image);
         TextView textView = (TextView) findViewById(R.id.textView);
-        //textView.setText(getResources().getStringArray(R.array.ru)[10 * category + number]);
-        DatabaseHandler databaseHandler = new DatabaseHandler(this);
-        databaseHandler.updateWordStats(1, new WordStats(7, 4));
-        try {
-            //textView.setText(Integer.toString(databaseHandler.getWordStats(2).getRight()) + "/" + Integer.toString(databaseHandler.getWordStats(2).getTotal()));
-            textView.setText(Integer.toString(databaseHandler.getAllStats().size()));
-        } catch (Exception e) {
-            textView.setText(e.getMessage());
+        cardNumber = 10 * category + number;
+        textView.setText(getResources().getStringArray(R.array.ru)[cardNumber]);
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            Intent intent = new Intent(getApplicationContext(), BackSideActivity.class);
+            intent.putExtra("number", cardNumber);
+            startActivity(intent);
         }
-        //databaseHandler.updateWordStats(0, new WordStats(1, 1));
+        return super.onTouchEvent(event);
     }
 }
